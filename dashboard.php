a6b51a3a3497b3ed02e06d2a5299e3f076201b21
igonzalez-et <igonzalezvirsa@gmail.com>
2023-01-13 19:23:45 +0100

    Login page

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@
 <body class="bodyDashboard">
     <?php include("./includes/header.php")?>
     <?php 
-        $_SESSION["user"] = ["user"=>"user","role"=>"profesor"];
+        $_SESSION["user"] = ["user"=>"user","role"=>"admin"];
         $rolUsuario = $_SESSION['user']['role'];
         if($rolUsuario == "admin"){
             include("./includes/dashboardAdmin.php");
