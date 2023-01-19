afbb3aea8dc526cc0230187e4170cac62a2ae0a2
R0sac <oscarhortelano2001@gmail.com>
2023-01-19 18:09:35 +0100

    destroy

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
 <div class="fixed-header">
-    <a href="./login.php"><button>Tancar sessió</button></a>
+    <a href="../loginDestroy.php"><button>Tancar sessió</button></a>
     <h1><a href="./dashboard.php">ENCUESTAS-IP</a></h1>
 </div>
