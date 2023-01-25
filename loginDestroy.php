fcfc5d1401149305361340018afdb71644e38619
R0sac <oscarhortelano2001@gmail.com>
2023-01-25 18:21:06 +0100

    acscas

--- loginDestroy.php
+++ loginDestroy.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
-session_destroy();
+include "./includes/log.php";
+try{
+    $user = $_SESSION["user"]["username"];
+    session_destroy();
+    unset($_POST["logout"]);
+    appendLog("S", "User " . $user . " has successfully logout");
+}catch (Exception $e){
+    $user = $_SESSION["user"]["username"];
+    appendLog("E", $e->getMessage());
+}
+// session_destroy();
 header("Location: ./login.php")
 ?>
