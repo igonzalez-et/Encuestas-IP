b77b7889ee6495a81de2943837dab005049793b5
R0sac <oscarhortelano2001@gmail.com>
2023-02-02 12:50:26 +0100

    fin 2

--- includes/log.php
+++ includes/log.php
@@ -1,7 +1,19 @@
 <?php
 function appendLog($messageTypeInitial,$message){
     $date = date("Y-m-d");
-    $log = "[" . $messageTypeInitial . "]" . " - " . "[". date("H:i:s") ."]" . " - " . "[" . $message . "]" . "\n";
+    $log = "[" . $messageTypeInitial . "]" . " - " . "[". date("H:i:s") ."]" . " - " . getIP()  . " - " . "[" . $message . "]" . "\n";
     file_put_contents("log/". "log_" . $date . ".txt", $log, FILE_APPEND);
 }
+
+function getIP(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
 ?>
